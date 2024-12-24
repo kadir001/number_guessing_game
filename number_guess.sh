0daa160f2e8a83f7b4a6bfbc181c147ed77168f9
Kadir Akdemir <kadirakdemir75@gmail.com>
2024-12-24 17:55:38 +0000

    feat: prompt for and check for username added

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,17 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+
+# Check if the user exists in the database
+USER_QUERY=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_QUERY ]]
+then
+  # If the user doesn't exist, welcome them and add to the database
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+else
+  # If the user exists, retrieve their data and welcome them back
+  IFS='|' read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_QUERY"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
